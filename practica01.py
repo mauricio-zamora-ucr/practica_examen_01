1ef15eb4a801126740156892fa764f75c16c30c4
Mauricio Andrés Zamora Hernández <64853106+mauricio-zamora-ucr@users.noreply.github.com>
2026-06-10 19:41:38 +0000

    Práctica

--- practica01.py
+++ practica01.py
@@ -10,26 +10,46 @@ lineas_padron: list[str] = [
     "901580698,814004, ,20360525,00000,DANIELA VANESSA               ,CHACON                    ,AGUILAR                   ",
     "901580721,814003, ,20360522,00000,MICHELLE MARIE                ,PEREZ                     ,ESPINOZA                  ",
     "901580735,103006, ,20360527,00000,NICHOLAS                      ,LOPEZ                     ,HIDALGO                   ",
-    "901580875,119075, ,20360518,00000,AUSTIN EDUARDO                ,SIBAJA                    ,VALVERDE                  "
+    "901580875,119075, ,20360518,00000,AUSTIN EDUARDO                ,SIBAJA                    ,VALVERDE                  ",
 ]
 
-def procesar_linea(texto:str) -> list[str]:
+
+def procesar_linea(texto: str) -> list[str]:
     """
     Esta función convierte una línea de texto en una lista de campos separados por
     coma. Además limpia cada campos de espacios a los extremos.
-    
+
     """
 
     # paso 1
     # convertir el texto en una lista usando split
-    lista:list[str] = texto.split(',')
+    lista: list[str] = texto.split(",")
     # paso 2
     # limpiar cada campo usando transformaciones con compresion del lista
     lista = [campo.strip() for campo in lista]
     return lista
 
 
-if __name__ == '__main__':
-    linea_ejemplo:str = lineas_padron[0]
-    lista:list[str] = procesar_linea(linea_ejemplo)
+def convertir_a_fecha_ISO(texto: str) -> str:
+    if len(texto) == 8:
+        yyyy: str = texto[:4]
+        mm: str = texto[4:6]
+        dd: str = texto[6:]
+        return yyyy + "-" + mm + "-" + dd
+    else:
+        return "0000-00-00"
+
+
+if __name__ == "__main__":
+    # Metodo 01
+    linea_ejemplo: str = lineas_padron[0]
+    lista: list[str] = procesar_linea(linea_ejemplo)
     print(lista)
+    # Metodo 02
+    fecha_sin_procesar1:str = lista[3]
+    fecha_sin_procesar2:str = '2026'
+    fecha_convertida1: str = convertir_a_fecha_ISO(fecha_sin_procesar1)
+    fecha_convertida2: str = convertir_a_fecha_ISO(fecha_sin_procesar2)
+    print(fecha_sin_procesar1,fecha_convertida1)
+    print(fecha_sin_procesar2,fecha_convertida2)
+
